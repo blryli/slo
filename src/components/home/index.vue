c7bde93dad9eaafc72b8164eb7904424e72d7f77
李志立 <405549416@qq.com>
2017-12-15 02:21:08 +0800

    数据模型构建

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="container m-t-20 p-b-10">
-      <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="300" infinite-scroll-immediate-check="true">
+      <div v-infinite-scroll="getCase" infinite-scroll-disabled="busy" infinite-scroll-distance="300" infinite-scroll-immediate-check="true">
         <case-list :case-arr="datas"></case-list>
       </div>
     </div>
@@ -14,6 +14,9 @@ var count = 0;
   export default {
     data () {
       return {
+    	page:1,
+    	pageSize:8,
+    	keyWords:'',
         datas: [],
         busy: false,
         caseArr: []
@@ -22,32 +25,30 @@ var count = 0;
     components: {
       caseList
     },
-    created () {
+    created(){
       this.getCase();
     },
     methods: {
-      loadMore() {
-        this.busy = true;
-        setTimeout(() => {
-          for (var i = 0, j = 8; i < j; i++) {
-            if(count < this.caseArr.length){
-              this.datas.push( this.caseArr[count] );
-              count++;
-            }
-          }
-          this.busy = false;
-        }, 1000);
-      },
-      getCase() {
-    	  this.$fns.post('/api/case/get-cases',{},(json)=>{
+      getCase(){
+    	  var data = {
+    			  page:this.page,
+    			  pageSize:this.pageSize,
+    			  keyWords:this.keyWords
+    	  }
+    	  this.$fns.post('/api/case/get-cases',data,(json)=>{
     		  if(json.ask=='1'){
-    			  this.datas = json.data
+            if(json.data.length){
+              this.page++;
+              json.data.forEach((item,k)=>{
+                this.datas.push(item);
+              })
+            }
     		  }else{
     			  console.error(json.message)
     		  }
     	  });
-      },
-    },
+      }
+    }
   }
 </script>
 
