c182ff110c9330e5c77b3cbbaa6ce27554d1e820
blry <405549416@qq.com>
2018-01-17 23:09:36 +0800

    xx

--- src/views/page/addCase.vue
+++ src/views/page/addCase.vue
@@ -1,51 +1,116 @@
 <template>
-  <el-row class="end-box">
-    <el-col :span="10">
-      <p><span class="span">所属公司：</span>
-        <el-select v-model="datas.name" clearable filterable placeholder="请选择">
-            <el-option
-              v-for="item in names"
-              :key="item.company_id"
-              :label="item.name"
-              :value="item.company_id">
-            </el-option>
-          </el-select>
-      </p>
-      <p><span class="span">标题：</span><el-input v-model="datas.title" placeholder=""></el-input></p>
-      <p><span class="span">作者：</span><el-input v-model="datas.author" placeholder=""></el-input></p>
-      <p><span class="span">业主单位：</span><el-input v-model="datas.unit" placeholder=""></el-input></p>
-    </el-col>
-    <el-col :span="10">
-      <p><span class="span">项目地址：</span><el-input v-model="datas.address"></el-input></p>
-      <p><span class="span">设计师团队：</span><el-input v-model="datas.team"></el-input></p>
-      <p><span class="span">用地规模(m²)：</span><el-input v-model="datas.scale"></el-input></p>
-      <p><span class="span">设计日期：</span>
-        <el-date-picker
-          v-model="datas.design_date"
-          type="date"
-          placeholder="选择日期">
-        </el-date-picker>
-      </p>
-      <p><span class="span">拍摄者：</span><el-input v-model="datas.photographer"></el-input></p>
-    </el-col>
-    <el-col :span="24">
-      <p><span class="span">案例描述：</span><vue-editor :editorToolbar="customToolbar" v-model="datas.desc"></vue-editor></p>
-        <div class="p">
-          <span class="span">案例图片：</span>
-          <div class="upload-img">
-            <el-upload action="/api/img/upload" list-type="picture-card" :name="name" ref="logos" :limit="logos.limit"  :multiple="logos.multiple"
-            :on-preview="preview" :on-remove="remove" :on-success="success">
-              <i class="el-icon-plus"></i>
-            </el-upload>
-            <el-dialog :visible.sync="dialogVisible" size="tiny">
-              <img width="100%" :src="dialogImageUrl" alt>
-            </el-dialog>
-          </div>
-        </div>
-      </p>
-      <p style="margin-top: 30px;"><span></span><el-button type="primary" @click="submit">提交</el-button></p>
-    </el-col>
-  </el-row>
+  <div class="end">
+    <el-row :gutter="10">
+      <el-col :span="8">
+        <el-input
+          placeholder="请输入内容"
+          v-model="imputValue"
+          clearable>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" @click.native="getCompanyList(isNew)">搜索</el-button>
+      </el-col>
+      <el-col :span="4" :offset="8">
+        <el-button type="success" @click.native="show = true">新建</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="公司名称"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        label="公司logo"
+        width="300">
+        <template slot-scope="scope">
+          <img :src="'/imgs/'+scope.row.logo" alt="logo">
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="公司邮箱"
+        width="300">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="total">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="pageSize"
+        :current-page="page"
+        @current-change="currentPage">
+      </el-pagination>
+    </div>
+
+    <div v-show="show">
+      <div class="particulars-close close-fixed" @click="show = false"></div>
+      <div class="end-bg"></div>
+      <el-row class="end-box">
+        <el-col :span="10">
+          <p><span class="span">所属公司：</span>
+            <el-select v-model="datas.name" clearable filterable placeholder="请选择">
+                <el-option
+                  v-for="item in names"
+                  :key="item.company_id"
+                  :label="item.name"
+                  :value="item.company_id">
+                </el-option>
+              </el-select>
+          </p>
+          <p><span class="span">标题：</span><el-input v-model="datas.title" placeholder=""></el-input></p>
+          <p><span class="span">作者：</span><el-input v-model="datas.author" placeholder=""></el-input></p>
+          <p><span class="span">业主单位：</span><el-input v-model="datas.unit" placeholder=""></el-input></p>
+        </el-col>
+        <el-col :span="10">
+          <p><span class="span">项目地址：</span><el-input v-model="datas.address"></el-input></p>
+          <p><span class="span">设计师团队：</span><el-input v-model="datas.team"></el-input></p>
+          <p><span class="span">用地规模(m²)：</span><el-input v-model="datas.scale"></el-input></p>
+          <p><span class="span">设计日期：</span>
+            <el-date-picker
+              v-model="datas.design_date"
+              type="date"
+              placeholder="选择日期">
+            </el-date-picker>
+          </p>
+          <p><span class="span">拍摄者：</span><el-input v-model="datas.photographer"></el-input></p>
+        </el-col>
+        <el-col :span="24">
+          <p><span class="span">案例描述：</span><vue-editor :editorToolbar="customToolbar" v-model="datas.desc"></vue-editor></p>
+            <div class="p">
+              <span class="span">案例图片：</span>
+              <div class="upload-img">
+                <el-upload action="/api/img/upload" list-type="picture-card" :name="name" ref="logos" :limit="logos.limit"  :multiple="logos.multiple"
+                :on-preview="preview" :on-remove="remove" :on-success="success">
+                  <i class="el-icon-plus"></i>
+                </el-upload>
+                <el-dialog :visible.sync="dialogVisible" size="tiny">
+                  <img width="100%" :src="dialogImageUrl" alt>
+                </el-dialog>
+              </div>
+            </div>
+          </p>
+          <p style="margin-top: 30px;"><span></span><el-button type="primary" @click="submit">提交</el-button></p>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -53,6 +118,12 @@ import { VueEditor } from 'vue2-editor'
 export default {
   data() {
     return {
+      show: false,
+      imputValue: '',
+      page: 1,
+      pageSize: 5,
+      total: 1,
+      tableData: [],
       datas: {
         "name":"",
         "title":"",
@@ -68,6 +139,7 @@ export default {
       dialogImageUrl: '',
       dialogVisible: false,
       name:'img',
+      logoImgList: [],
       logos: {
         limit:50,
         multiple:true,
@@ -96,6 +168,66 @@ export default {
   	
   },
   methods: {
+    handleEdit(index, row) {
+      this.show = true;
+      var data = {
+        companyId: row.company_id
+      }
+      this.$fns.post('/api/admin/get-company',data,(json)=>{
+          if(json.ask=='1'){
+            let arr = [];
+            let imgArr = [];
+            this.logoImgList = [];
+            this.datas = json.data;
+            json.data.imgs.forEach((d, i) => {
+              arr.push({url: '/imgs/' + d});
+              imgArr.push('/imgs/' + d);
+            })
+            //展示图片
+            this.logoImgList.push({url: '/imgs/' + json.data.logo});
+            this.companyImgList = arr;
+            //上传图片
+            this.logoImg.imgs = []; 
+            this.logoImg.imgs[0] = '/imgs/' +json.data.logo;
+            this.companyImg.imgs = imgArr;
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
+    handleDelete(index, row) {
+      var data = {
+        company_id: row.company_id
+      }
+      this.$fns.post('/api/admin/delete-company',data,(json)=>{
+        if(json.ask=='1'){
+          this.$message({message:'操作成功！',type:'success',showClose:true});
+          this.tableData.splice(index, 1);
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
+    currentPage(page) {
+      this.page = page;
+      this.getCompanyList();
+    },
+    getCompanyList(isNew) {
+      var _this = this;
+      var data = {
+        name: this.imputValue,
+        pageSize: this.pageSize,
+        page: this.page
+      }
+      this.$fns.post('/api/admin/company-list',data,(json)=>{
+          if(json.ask=='1'){
+            _this.tableData = json.data;
+            _this.total = parseInt(json.total);
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
 	  submit() {
       var data = {
               company_id: this.datas.name,
@@ -178,7 +310,50 @@ export default {
 </script>
 
 <style lang="scss">
+//关闭按钮
+.particulars-close{
+    position: absolute;
+    right: 15px;
+    top: -36px;
+    width: 30px;
+    height: 30px;
+    z-index: 100;
+    color: #fff;
+    font-weight: normal;
+    cursor: pointer;
+    background: url(/static/img/ic_close.png) no-repeat center center;
+    &:hover{
+        background: url(/static/img/ic_close_2.png) no-repeat center center;
+    }
+}
+.cell img{
+  width: 50px;
+  height: 50px;
+}
+.total{
+  text-align: right;
+  margin-top: 20px;
+}
+.end-bg{
+  background-color: #000;
+  opacity: .5;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+}
 .end-box{
+  overflow: scroll;
+  padding: 20px;
+  position: fixed;
+  top: 10%;
+  left: 5%;
+  z-index: 100;
+  width: 90%;
+  height: 80%;
+  background-color: #fff;
   input[type=file] {
       display: none !important;
   }
