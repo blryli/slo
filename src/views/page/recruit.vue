0e0b8a38b6cfdd801c2987ac7af764e57d9d6494
blry <405549416@qq.com>
2018-01-30 21:33:52 +0800

    xx

--- src/views/page/recruit.vue
+++ src/views/page/recruit.vue
@@ -1,20 +1,83 @@
 <template>
-    <div class="end-box">
-      <p><span class="span">公司名称：</span>
-        <el-select v-model="datas.name" clearable filterable placeholder="请选择公司">
-            <el-option
-              v-for="item in names"
-              :key="item.company_id"
-              :label="item.name"
-              :value="item.company_id">
-            </el-option>
-          </el-select>
-      </p>
-      <p><span class="span">标题：</span><el-input v-model="datas.title" placeholder=""></el-input></p>
-      <p><span class="span">职位：</span><el-input v-model="datas.position"></el-input></p>
-      <p><span class="span">招聘描述：</span><vue-editor :editorToolbar="customToolbar" v-model="datas.desc"></vue-editor></p>
-      <p style="margin-top: 30px;"><span></span><el-button type="primary" @click="submit">提交</el-button></p>
+  <div class="end">
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <el-form :inline="true" class="demo-form-inline">
+          <el-form-item>
+          <el-input placeholder="公司名称" v-model="imputValue" clearable></el-input>
+          </el-form-item>
+          <el-form-item>
+          <el-input placeholder="标题" v-model="imputTitleValue" clearable></el-input>
+          </el-form-item>
+          <el-button type="primary" @click.native="getCompanyList(isNew)">搜索</el-button>
+        </el-form>
+      </el-col>
+      <el-col :span="4" :offset="8">
+        <el-button type="success" @click.native="show = true">新建</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="company_name"
+        label="公司名称"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="title"
+        label="标题"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="position"
+        label="职位"
+        width="300">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="total">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="pageSize"
+        :current-page="page"
+        @current-change="currentPage">
+      </el-pagination>
     </div>
+    <div v-show="show">
+      <div class="particulars-close close-fixed" @click="show = false"></div>
+      <div class="end-bg"></div>
+      <div class="end-box">
+        <p><span class="span">公司名称：</span>
+          <el-select v-model="datas.company_id" clearable filterable placeholder="请选择公司">
+              <el-option
+                v-for="item in names"
+                :key="item.company_id"
+                :label="item.company_name"
+                :value="item.company_id">
+              </el-option>
+            </el-select>
+        </p>
+        <p><span class="span">标题：</span><el-input v-model="datas.title" placeholder=""></el-input></p>
+        <p><span class="span">职位：</span><el-input v-model="datas.position"></el-input></p>
+        <p><span class="span">招聘描述：</span><vue-editor :editorToolbar="customToolbar" v-model="datas.desc"></vue-editor></p>
+        <p style="margin-top: 30px;"><span></span><el-button type="primary" @click="submit">提交</el-button></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,8 +85,17 @@ import { VueEditor } from 'vue2-editor'
 export default {
   data() {
     return {
+      isNew: true,
+      show: false,
+      imputValue: '',
+      imputTitleValue: '',
+      page: 1,
+      pageSize: 5,
+      total: 1,
+      tableData: [],
       datas: {
-        "name":"",
+        "recruitment_id":1,
+        "company_id":"",
         "title":"",
         "position":"",
         "desc":""
@@ -48,6 +120,55 @@ export default {
     this.getCompanys();
   },
   methods: {
+    //编辑
+    handleEdit(index, row) {
+      this.show = true;
+      var data = {
+        recruitment_id: row.recruitment_id
+      }
+      this.$fns.post('/api/admin/get-company',data,(json)=>{
+          if(json.ask=='1'){
+            this.datas = json.data;
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
+    //删除
+    handleDelete(index, row) {
+      var data = {
+        recruitment_id: row.recruitment_id
+      }
+      this.$fns.post('/api/admin/delete-recruitment',data,(json)=>{
+        if(json.ask=='1'){
+          this.$message({message:'操作成功！',type:'success',showClose:true});
+          this.tableData.splice(index, 1);
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
+    currentPage(page) {
+      this.page = page;
+      this.getCompanyList();
+    },
+    getCompanyList(isNew) {
+      var _this = this;
+      var data = {
+        companyId: this.imputValue,
+        imputTitleValue: this.recruitment_id,
+        pageSize: this.pageSize,
+        page: this.page
+      }
+      this.$fns.post('/api/admin/recruitment-list',data,(json)=>{
+          if(json.ask=='1'){
+            _this.tableData = json.data;
+            _this.total = parseInt(json.total);
+          }else{
+            this.$message({message:json.message,type:'error',showClose:true});
+          }
+      });
+    },
     submit() {
       var data = {
               company_id: this.datas.name,
@@ -94,7 +215,56 @@ export default {
 </script>
 
 <style lang="scss">
+.v-modal{
+  z-index: 90 !important;
+}
+.cell img{
+  width: 50px;
+  height: 50px;
+}
+//关闭按钮
+.particulars-close{
+    position: absolute;
+    right: 15px;
+    top: -36px;
+    width: 30px;
+    height: 30px;
+    z-index: 100;
+    color: #fff;
+    font-weight: normal;
+    cursor: pointer;
+    background: url(/static/img/ic_close.png) no-repeat center center;
+    &:hover{
+        background: url(/static/img/ic_close_2.png) no-repeat center center;
+    }
+}
+.total{
+  text-align: right;
+  margin-top: 20px;
+}
+.end-bg{
+  background-color: #000;
+  opacity: .5;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+}
 .end-box{
+  overflow: scroll;
+  padding: 20px;
+  position: fixed !important;
+  top: 10%;
+  left: 5%;
+  z-index: 100;
+  width: 90%;
+  height: 80%;
+  background-color: #fff;
+  input[type=file] {
+      display: none !important;
+  }
   input[type=file] {
       display: none !important;
   }
